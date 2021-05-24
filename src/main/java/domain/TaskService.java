b70275ec5724c20020ac9fc1985cd04482ad5746
kamenova92 <tkamenova92@gmail.com>
2021-05-24 15:00:27 +0200

    Test for creating a new task

--- src/main/java/domain/TaskService.java
+++ src/main/java/domain/TaskService.java
@@ -1,29 +1,28 @@
 package domain;
 
+import domain.dtos.TaskDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import static java.time.LocalDateTime.now;
-import static java.util.Collections.singletonList;
 
 @Service
 public class TaskService {
 
-    private final Task task;
     private final TaskAssignee taskAssignee;
 
     @Autowired
-    public TaskService(Task task,
-                       TaskAssignee taskAssignee
-    ) {
-        this.task = task;
+    public TaskService(TaskAssignee taskAssignee) {
         this.taskAssignee = taskAssignee;
     }
 
-    public Task createTask(Task task, TaskAssignee assignee) {
-        task.setTittle("New task");
-        task.setDescription("Description");
-        task.setStartDateTime(now());
-        task.setAssigneeList(singletonList(assignee));
+
+    public Task createTask(TaskDto taskDto) {
+        Task task = new Task(taskDto.getId(),
+                            taskDto.getTittle(),
+                            taskDto.getStartDateTime(),
+                            taskDto.getEndDateTime(),
+                            taskDto.getTaskNotificationSeries(),
+                            taskDto.getAssigneeList()
+        );
         return task;
     }
 }
---
+++ src/test/java/domain/TaskServiceTest.java
@@ -0,0 +1,21 @@
+package domain
+import domain.dtos.TaskDto;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public class TaskServiceTest {
+    @InjectMocks TaskService testee;
+
+   @Mock private TaskDto taskDto;
+
+    @Test
+    public void test_createTask_shouldReturn_newTask() {
+      Task newTask = testee.createTask(taskDto);
+
+        assert newTask != null;
+    }
+}
